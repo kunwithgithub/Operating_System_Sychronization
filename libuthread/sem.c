bb9adf73cfcbe082c2aabad0c233e074e3e28423
Yinglei Tan <llltan@ucdavis.edu>
2019-05-09 19:26:33 -0700

    Add files via upload

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -6,119 +6,94 @@
 #include "thread.h"
 
 struct semaphore {
-	
-	int count;
-	queue_t waiting;
-
+  
+  int count;
+  queue_t waiting;
+  
 };
 
 sem_t sem_create(size_t count)
 {
-	sem_t sem = (sem_t)malloc(sizeof(struct semaphore));
-	if (sem == NULL){
-		return NULL;
-	}
-	sem->count = count;
-	sem->waiting = queue_create();
-	return sem;
+  sem_t sem = (sem_t)malloc(sizeof(struct semaphore));
+  if (sem == NULL){
+    return NULL;
+  }
+  enter_critical_section();
+  sem->count = count;
+  sem->waiting = queue_create();
+  exit_critical_section();
+  return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-	if(sem == NULL || (queue_length(sem->waiting) > 0)){
-		return -1;
-	}
-	queue_destroy(sem->waiting);
-	free(sem);
-	return 0;
+  enter_critical_section();
+  if(sem == NULL || (queue_length(sem->waiting) > 0)){
+    return -1;
+  }
+  queue_destroy(sem->waiting);
+  free(sem);
+  exit_critical_section();
+  return 0;
 }
 
-/*void sem_down(sem)
-{
-spinlock_acquire(sem->lock);
-while (sem->count == 0) {
-// Block self 
-...
-}
-sem->count -= 1;
-spinlock_release(sem->lock);
-}*/
 
 int sem_down(sem_t sem)
 {
-	if(sem == NULL){
-		return -1;
-	}
-    
-    enter_critical_section();
-	while (sem->count == 0) {
-        pthread_t TID = pthread_self();
-        queue_enqueue(waiting, (void*)TID); //
-		thread_block();
-	}
-    sem->count -= 1;
-    exit_critical_section();
-    
-    return 0;
-	
+  if(sem == NULL){
+    return -1;
+  }
+  
+  enter_critical_section();
+  while (sem->count == 0) {
+    pthread_t TID = pthread_self();
+    queue_enqueue(sem->waiting, (void*)TID); //
+    thread_block();
+  }
+  sem->count -= 1;
+  exit_critical_section();
+  
+  return 0;
+  
 }
 
-/*void sem_up(sem)
-{
-spinlock_acquire(sem->lock);
-sem->count += 1;
- //Wake up first in line 
-	//(if any) 
-...
-spinlock_release(sem->lock);
-} */
 
 int sem_up(sem_t sem)
 {
-    if(sem == NULL){
-        return -1;
-    }
+  if(sem == NULL){
+    return -1;
+  }
+  
+  enter_critical_section();
+  sem->count += 1;
+  
+  if(queue_length(sem->waiting)>0){
     pthread_t tempTID;
-    enter_critical_section();
-    if(queue_length(sem->waiting)>0){
-        sem->count += 1;
-        queue_dequeue(sem->waiting, (void**)&tempTID);
-        thread_unblock(tempTID);
-    }
-    exit_critical_section();
-    return 0;
+    
+    queue_dequeue(sem->waiting, (void**)&tempTID);
+    thread_unblock(tempTID);
+   
+  }
+  
+  exit_critical_section();
+  return 0;
 }
 
-/*
- * sem_getvalue - Inspect semaphore's internal state
- * @sem: Semaphore to inspect
- * @sval: Address of data item where value is received
- *
- * If semaphore @sem's internal count is greater than 0, assign internal count
- * to data item pointed by @sval.
- *
- * If semaphore @sems's internal count is equal to 0, assign a negative number
- * whose absolute value is the count of the number of threads currently blocked
- * in sem_down().
- *
- * Return: -1 if @sem or @sval are NULL. 0 if semaphore was successfully
- * inspected.
- */
 int sem_getvalue(sem_t sem, int *sval)
 {
-	if(sem == NULL || sval == NULL){
-		return -1;
-	}
-    
-    enter_critical_section();
-	if(sem->count > 0){
-		sval = sem->count;
-	}
-	else if (sem->count == 0){
-		sval = -1*(queue_length(sem->waiting));
-	}
-    exit_critical_section();
-    
-	return 0;
+  if(sem == NULL || sval == NULL){
+    return -1;
+  }
+  
+  enter_critical_section();
+  if(sem->count > 0){
+    *sval = sem->count;
+  }
+  else if (sem->count == 0){
+    *sval = -1*(queue_length(sem->waiting));
+  }
+  exit_critical_section();
+  
+  return 0;
 }
 
