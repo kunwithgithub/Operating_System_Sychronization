fec76112a986e11a8852a60d1376dab140a5a75b
kunwithgithub <45131670+kunwithgithub@users.noreply.github.com>
2019-05-11 16:36:01 -0700

    Update tps.c

--- test/tps.c
+++ test/tps.c
@@ -1,109 +1,193 @@
 #include <assert.h>
-#include <limits.h>
 #include <pthread.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#include <tps.h>
-#include <sem.h>
+#include "queue.h"
+#include "thread.h"
+#include "tps.h"
 
-static char msg1[TPS_SIZE] = "Hello world!\n";
-static char msg2[TPS_SIZE] = "hello world!\n";
+/* TODO: Phase 2 */
+queue_t TPSs;
 
-static sem_t sem1, sem2;
+struct TPS{
+	pthread_t tid;
+	struct page *privateMemoryPage; 
+};
 
-void *thread2(void* arg)
+struct page{
+	void *pageAddress; // phase 3
+	int referenceNumber;
+};
+
+int find_item(void *data, void *arg)
 {
-	char *buffer = malloc(TPS_SIZE);
+    pthread_t tid = (*(pthread_t*)arg);
+  
+    if (tid == ((struct TPS*)data)->tid)
+    {
+        return 1;
+    }
 
-	/* Create TPS and initialize with *msg1 */
-	tps_create();
-	tps_write(0, TPS_SIZE, msg1);
-
-	/* Read from TPS and make sure it contains the message */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread2: read OK!\n");
-
-	/* Transfer CPU to thread 1 and get blocked */
-	sem_up(sem1);
-	sem_down(sem2);
-
-	/* When we're back, read TPS and make sure it sill contains the original */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread2: read OK!\n");
-
-	/* Transfer CPU to thread 1 and get blocked */
-	sem_up(sem1);
-	sem_down(sem2);
-
-	/* Destroy TPS and quit */
-	tps_destroy();
-	return NULL;
+    return 0;
 }
 
-void *thread1(void* arg)
+int find_fault(void *data, void *arg)
 {
-	pthread_t tid;
-	char *buffer = malloc(TPS_SIZE);
+	
+  
+    if (arg == ((struct TPS*)data)->privateMemoryPage)
+    {
+        return 1;
+    }
 
-	/* Create thread 2 and get blocked */
-	pthread_create(&tid, NULL, thread2, NULL);
-	sem_down(sem1);
-
-	/* When we're back, clone thread 2's TPS */
-	tps_clone(tid);
-
-	/* Read the TPS and make sure it contains the original */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!memcmp(msg1, buffer, TPS_SIZE));
-	printf("thread1: read OK!\n");
-
-	/* Modify TPS to cause a copy on write */
-	buffer[0] = 'h';
-	tps_write(0, 1, buffer);
-
-	/* Transfer CPU to thread 2 and get blocked */
-	sem_up(sem2);
-	sem_down(sem1);
-
-	/* When we're back, make sure our modification is still there */
-	memset(buffer, 0, TPS_SIZE);
-	tps_read(0, TPS_SIZE, buffer);
-	assert(!strcmp(msg2, buffer));
-	printf("thread1: read OK!\n");
-
-	/* Transfer CPU to thread 2 */
-	sem_up(sem2);
-
-	/* Wait for thread2 to die, and quit */
-	pthread_join(tid, NULL);
-	tps_destroy();
-	return NULL;
+    return 0;
+	
 }
 
-int main(int argc, char **argv)
+static void segv_handler(int sig, siginfo_t *si, void *context)
 {
-	pthread_t tid;
+    /*
+     * Get the address corresponding to the beginning of the page where the
+     * fault occurred
+     */
+    void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
 
-	/* Create two semaphores for thread synchro */
-	sem1 = sem_create(0);
-	sem2 = sem_create(0);
+    /*
+     * Iterate through all the TPS areas and find if p_fault matches one of them
+     */
+	struct TPS *foundTPS;
+	int match = queue_iterate(TPSs,find_fault,p_fault,(void **)&foundTPS);
+    if (match != -1){
+        /* Printf the following error message */
+		if(foundTPS != NULL){
+			fprintf(stderr, "TPS protection error!\n");
+		}
+	}
+    /* In any case, restore the default signal handlers */
+    signal(SIGSEGV, SIG_DFL);
+    signal(SIGBUS, SIG_DFL);
+    /* And transmit the signal again in order to cause the program to crash */
+    raise(sig);
+}
 
-	/* Init TPS API */
-	tps_init(1);
+int tps_init(int segv)
+{
+	/* TODO: Phase 2 */
+	if(TPSs != NULL){
+		return -1;
+	}
+	
+	TPSs = queue_create();
+	
+	if(TPSs == NULL){
+		return -1;
+	}
+	if (segv) {
+        struct sigaction sa;
 
-	/* Create thread 1 and wait */
-	pthread_create(&tid, NULL, thread1, NULL);
-	pthread_join(tid, NULL);
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = SA_SIGINFO;
+        sa.sa_sigaction = segv_handler;
+        sigaction(SIGBUS, &sa, NULL);
+        sigaction(SIGSEGV, &sa, NULL);
+    }
 
-	/* Destroy resources and quit */
-	sem_destroy(sem1);
-	sem_destroy(sem2);
+	return 0;
+}
+
+int tps_create(void)
+{
+	/* TODO: Phase 2 */
+	int queueSize = queue_length(TPSs);
+	struct TPS *newTPS = (struct TPS*)malloc(TPS_SIZE);
+	if(newTPS == (void*)-1){
+		return -1;
+	}
+	
+	struct page *newPage = mmap(NULL,sizeof(struct page),PROT_NONE,MAP_ANONYMOUS,-1,0);
+	newTPS->privateMemoryPage = (struct page*)malloc(sizeof(struct page));	
+	newTPS->privateMemoryPage->pageAddress = newPage;
+	newTPS->tid = pthread_self();
+	queue_enqueue(newTPS);
+	return 0;
+}
+
+
+int tps_destroy(void)
+{
+	/* TODO: Phase 2 */
+	phread_t currentTid;
+	currentTid = pthread_self();
+	struct TPS *currentTPS;
+	int success = queue_iterate(TPSs,find_item,(void *)currentTid,(void **)&currentTPS);
+	if(currentTPS==NULL||success==-1){
+		return -1;
+	}
+	munmap(currentTPS->privateMemoryPage->pageAddress,sizeof(struct page));
+	queue_delete(TPSs,currentTPS);
+	free(currentTPS);
+	return 0;
+}
+
+int tps_read(size_t offset, size_t length, char *buffer)
+{
+	/* TODO: Phase 2 */
+	pthread_t currentTid = pthread_self();
+	struct TPS *currentThread;
+	int sucess = queue_iterate(TPSs,find_item,(void *)currentTid,(void **)&currentThread);
+	if(success == -1 || currentThread == NULL||offset+length>TPS_SIZE||buffer == NULL){
+		return -1;
+	}
+	mprotect(currentThread->privateMemoryPage->pageAddress,sizeof(struct page),PROT_READ);
+	memcpy((void *)buffer, currentThread+offset,length);
+	return 0;
+}
+
+int tps_write(size_t offset, size_t length, char *buffer)
+{
+	/* TODO: Phase 2 */
+	pthread_t currentTid = pthread_self();
+	struct TPS *currentThreadTPS;
+	int sucess = queue_iterate(TPSs,find_item,(void *)currentTid,(void **)&currentThreadTPS);
+	if(success == -1 || currentThreadTPS == NULL||offset+length>TPS_SIZE||buffer == NULL){
+		return -1;
+	}
+	if(currentThreadTPS->privateMemoryPage->referenceNumber>1){
+		struct page *newPage = mmap(NULL,sizeof(struct page),PROT_NONE,MAP_ANONYMOUS,-1,0);
+	}
+	mprotect(currentThreadTPS->privateMemoryPage->pageAddress, sizeof(struct page),PROT_WRITE);
+	memcpy(currentThreadTPS+offset,(void *)buffer,length);
+	
+	return 0;
+}
+
+int tps_clone(pthread_t tid)
+{
+	/* TODO: Phase 2 */
+	pthread_t currentTid = pthread_self();
+	struct TPS *willBeCloned;
+	struct TPS *currentThread;
+	int sucess = queue_iterate(TPSs,find_item,(void *)tid,(void **)&willBeCloned);
+	int anotherSuccess = queue_iterate(TPSs,find_item,(void *)currentTid,(void **)&currentThread);
+	if(willBeCloned == NULL || success == -1|| anotherSuccess==-1 ||currentThread!=NULL||willBeCloned==NULL){
+		return -1;
+	}
+	struct TPS *newTPS = (struct TPS*)malloc(sizeof(struct TPS));
+	int queueSize = queue_length(TPSs);
+	newTPS->tid = currentTid;
+	/*	phase 2
+	newTPS->privateMemoryPage = (struct page*)malloc(sizeof(struct page);
+	newTPS->privateMemoryPage->pageAddress = mmap(NULL,sizeof(struct page),PROT_EXEC|PROT_READ|PROT_WRITE,MAP_ANONYMOUS,-1,0);
+	memcpy(newTPS,willBeCloned,TPS_SIZE);
+	*/
+	newTPS->privateMemoryPage = willBeCloned->privateMemoryPage; //phase 3
+	newTPS->privateMemoryPage->referenceNumber++; //phase 3
+	queue_enqueue(newTPS);
 	return 0;
 }
